ddd7e98e742c2979caefe27ea1fc1fe8d24b4439
MacRawat58 <96304497+MacRawat58@users.noreply.github.com>
2024-05-24 22:33:16 +0530

    login and diffrent routes created in dashboard

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Dashboard from "./components/Dashboard/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
+import Home from "./components/Dashboard/Home";
+import IPSpeakerConfiguration from "./components/Dashboard/IPSpeakerConfiguration";
+import AudioSettings from "./components/Dashboard/AudioSettings";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        >
+          <Route path="home" element={<Home />} />
+          <Route
+            path="ip-speaker-configuration"
+            element={<IPSpeakerConfiguration />}
+          />
+          <Route path="audio-settings" element={<AudioSettings />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
